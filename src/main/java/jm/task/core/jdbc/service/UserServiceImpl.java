16fece04d8059e6486ba03b48e53bd5cea795759
Skymiss90 <93150288+Skymiss90@users.noreply.github.com>
2023-06-04 15:42:55 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,35 +8,35 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    //private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-    UserDao userDao = new UserDaoHibernateImpl();
+    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    //UserDao userDao = new UserDaoHibernateImpl();
     public void createUsersTable() {
-        //userDaoJDBC.createUsersTable();
-        userDao.createUsersTable();
+        userDaoJDBC.createUsersTable();
+       // userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-    // userDaoJDBC.dropUsersTable();
-        userDao.dropUsersTable();
+        userDaoJDBC.dropUsersTable();
+        //userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        //userDaoJDBC.saveUser(name, lastName, age);
-        userDao.saveUser(name, lastName, age);
+        userDaoJDBC.saveUser(name, lastName, age);
+        //userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        //userDaoJDBC.removeUserById(id);
-        userDao.removeUserById(id);
+        userDaoJDBC.removeUserById(id);
+        //userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        //return userDaoJDBC.getAllUsers();
-        return userDao.getAllUsers();
+        return userDaoJDBC.getAllUsers();
+        //return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        //userDaoJDBC.cleanUsersTable();
-        userDao.cleanUsersTable();
+        userDaoJDBC.cleanUsersTable();
+        //userDao.cleanUsersTable();
     }
 }
